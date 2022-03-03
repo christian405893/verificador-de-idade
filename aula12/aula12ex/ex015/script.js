f406bd133aa40f4cd1afd829469502d26cfe6f61
Christian <8698166990c.f.s.b@gmail.com>
2022-03-03 03:10:44 -0300

    update

--- aula12/aula12ex/ex015/script.js
+++ aula12/aula12ex/ex015/script.js
@@ -1,3 +1,4 @@
+console.log("Teste")
 function verificar (){
     var data = new Date()
     var ano = data.getFullYear()
@@ -10,11 +11,43 @@ function verificar (){
         var fsex =  document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
+            if(idade >= 0 && idade <=15){
+                //criança
+                img.setAttribute('src', 'bebem.jpg')
+            } else if ( idade <= 18){
+                //adolecente
+                img.setAttribute('src', 'jovemm.jpg')
+            } else if (idade > 18 && idade < 50) {
+                //adulto
+                img.setAttribute('src', 'atultom.jpg')
+            } else {
+                //ancião(ã)
+                img.setAttribute('src', 'idoso.jpg')
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if(idade >= 0 && idade <=15){
+                //criança
+                img.setAttribute('src', 'bebef.jpg')
+            } else if ( idade <= 18){
+                //adolecente
+                img.setAttribute('src', 'jovemf.jpg')
+            } else if (idade > 18 && idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adultof.jpg')
+            } else {
+                //ancião(ã)
+                img.setAttribute('src', 'idosa.jpg')
+            }
         }
+        res.style.textAling = 'center'
        res.innerHTML = `detectamos ${genero} com ${idade} anos.`
+       res.appendChild(img).style.borderRadius
+       //<img id="foto" style="border-radius: 50%" width="250" height="250"
     }
 }
